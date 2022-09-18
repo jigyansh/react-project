c98cd9024c4850c7db153b790374e863cfbc4eae
jigyansh <61785151+jigyansh@users.noreply.github.com>
2022-09-18 15:00:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Feed from "./Feed";
 import Header from "./header";
 import Sidebar from "./Sidebar";
 import Login from "./Login.js";
-
+ 
 function App() {  
   const user = null;
   return (
